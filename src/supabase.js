fe74c5b2f41101cf15e63dd1c0d4175d62559fd4
Srushti <srushtisarts@gmail.com>
2026-06-18 17:26:45 +0530

    Added Supabase integration and product management features

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY
+  "https://ukjsslkmgzkgqbyzdxnp.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVranNzbGttZ3prZ3FieXpkeG5wIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTU3ODA0MiwiZXhwIjoyMDk3MTU0MDQyfQ.jOwlyKNNPs2KA4YrFsfl0XJqn35tgwIGQy1XQZN5YPs"
 );
+
+
+// createClient(
+//   process.env.SUPABASE_URL,
+//   process.env.SUPABASE_SECRET_KEY
